4038a3cc51e745e53e09bf014d24de8cacb86683
Shaikh Sarfaraj <64125127+iamsrshaikh@users.noreply.github.com>
2021-02-07 18:09:57 +0530

    Create Max_Heap.cpp

---
+++ Heap/Max_Heap.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+
+using namespace std;
+
+void insert(int A[],int n){
+    int i=n;
+    int temp;
+    temp=A[i];
+    
+    while(i>1 && temp>A[i/2]){
+        A[i]=A[i/2];
+        i=i/2;
+    }
+    A[i]=temp;
+    
+}
+
+
+
+
+int main(){
+    int n;
+    cout<<"Enter the size of Array: "<<endl;
+    cin>>n;
+    
+    int A[n];
+    
+    cout<<"Enter the elements: "<<endl;
+    
+    for(int i=1;i<=n;i++){
+        cin>>A[i];
+    }
+    
+    for(int i=2;i<=n;i++){
+        insert(A,i);
+    }
+    
+    cout<<"Max Heap: ";
+    for(int i=1;i<=n;i++){
+        cout<<A[i]<<" ";
+    }
+    cout<<endl;
+    
+    
+    
+}
